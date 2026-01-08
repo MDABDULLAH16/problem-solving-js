28b0bc90df41f18e08424b7c06fdd5006b537672
MD ABDULLAH <mdabdullah161036@gmail.com>
2026-01-08 17:35:19 +0600

    vowel count

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 
 const reversed = (str) => [...str].reverse().join('');
 // Input: "hello"
-const result = reversed('hello');
-console.log(result);
+const result1 = reversed('hello');
+console.log(result1);
 // Output: "olleh"
+
+
+// Problem 2: Count Vowels in a String
+// Write a function that counts how many vowels (a, e,i, o, u) are in a given string.
+const countVowels = (str) => {
+    const arr = [...str];
+    const vowels = 'aeiou'
+    let count = 0;
+ for (const alfa of arr) {
+       if (vowels.includes(alfa)) {
+        count++
+       }
+       
+ }
+return count
+}
+ 
+// Input: "programming"
+const result2 = countVowels("programming");
+// Output: 3
+console.log(result2);
